77c6a1d2b7afbb9de6955360e3879c1a8d3f6943
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-04-20 15:55:25 +0600

    order page completed

--- src/pages/OrdersPage.tsx
+++ src/pages/OrdersPage.tsx
@@ -3,6 +3,9 @@ import { motion } from "framer-motion";
 
 import Header from "../components/common/Header";
 import StatCard from "../components/common/StatCard";
+import DailyOrders from "../components/orders/DailyOrders";
+import OrderDistribution from "../components/orders/OrderDistribution";
+import OrdersTable from "../components/orders/OrdersTable";
 
 const orderStats = {
 	totalOrders: "1,234",
